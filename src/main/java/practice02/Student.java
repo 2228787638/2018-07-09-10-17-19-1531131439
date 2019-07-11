025f292cc5c159a5c79c60ed4a75f20301edd6d6
2228787638 <2228787638@qq.com>
2019-07-11 19:58:20 +0800

    feat: implement practice02

--- src/main/java/practice02/Student.java
+++ src/main/java/practice02/Student.java
@@ -2,4 +2,26 @@ package practice02;
 
 public class Student extends Person{
 
+    private int klass;
+
+    public Student(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public void setKlass(int klass) {
+        this.klass = klass;
+    }
+
+    public Student(String name, int age) {
+        super(name, age);
+    }
+    @Override
+    public String introduce(){
+        return "I am a Student. I am at Class 2.";
+    }
 }
